5e5f66d97346e5fd658d9b5cdb426f8e7fea0de0
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2015-02-26 19:10:19 -0500

    [#13] print DEBUG info on error
    - if 0 available versions throw error
    - include URL to GH issues

--- bin/m.js
+++ bin/m.js
@@ -5,20 +5,23 @@ var mvm = require('../'),
   fs = require('fs'),
   docopt = require('docopt').docopt,
   pkg = require('../package.json'),
-  argv = docopt(fs.readFileSync(__dirname + '/m.docopt', 'utf-8'), {version: pkg.version}),
+  argv = docopt(fs.readFileSync(__dirname + '/m.docopt', 'utf-8'), {
+    version: pkg.version
+  }),
   spawn = require('child_process').spawn,
   _ = require('underscore'),
   debug = require('debug')('mongodb-version-manager');
 
-function printVersions(versions, fn){
-  mvm.current(function(err, current){
-    if(err) return fn(err);
+var cmd;
 
-    console.log(versions.map(function(v){
-      if(v === current){
-        return ' \033[32mο '+v+'\033[0m \033[90m \033[0m';
-      }
-      else {
+function printVersions(versions, fn) {
+  mvm.current(function(err, current) {
+    if (err) return fn(err);
+
+    console.log(versions.map(function(v) {
+      if (v === current) {
+        return ' \033[32mο ' + v + '\033[0m \033[90m \033[0m';
+      } else {
         return '   ' + v + '\033[90m \033[0m';
       }
     }).join('\n'));
@@ -27,27 +30,40 @@ function printVersions(versions, fn){
   });
 }
 
-var abortIfError = function(err){
-  if(err) return console.error(err) && process.exit(1);
+var abortIfError = function(err) {
+  if (err) {
+    console.error(err);
+    console.error('----');
+    console.error('Try running your command again with debugging on:');
+    console.error('  DEBUG=* m ' + cmd);
+    console.error('Bug reports welcome at http://github.com/imlucas/mongodb-version-manager');
+    console.error('----');
+    return process.exit(1);
+  }
 };
 
 var commands = {
-  available: function(){
+  available: function() {
     var title = '';
-    if(!argv['--rc'] && !argv['--stable'] && !argv['--unstable']){
+    if (!argv['--rc'] && !argv['--stable'] && !argv['--unstable']) {
       argv['--all'] = true;
     }
-    if(argv['--all']){
+    if (argv['--all']) {
       argv['--stable'] = true;
       argv['--unstable'] = true;
       argv['--rc'] = true;
       title = 'All';
-    }
-    else {
+    } else {
       var includes = [];
-      if(argv['--stable']) includes.push('Stable');
-      if(argv['--unstable']) includes.push('Unstable');
-      if(argv['--rc']) includes.push('Release Candidates');
+      if (argv['--stable']) {
+        includes.push('Stable');
+      }
+      if (argv['--unstable']) {
+        includes.push('Unstable');
+      }
+      if (argv['--rc']) {
+        includes.push('Release Candidates');
+      }
       title = includes.join('|');
     }
 
@@ -58,65 +74,77 @@ var commands = {
       pokemon: argv['--pokemon']
     };
 
-    mvm.installed(function(err, installed){
+    mvm.installed(function(err, installed) {
       abortIfError(err);
 
-      mvm.available(opts, function(err, versions){
+      mvm.available(opts, function(err, versions) {
         abortIfError(err);
 
-        if(opts.pokemon){
+        if (!versions || versions.length === 0) {
+          return abortIfError(new Error('No available versions?'));
+        }
+
+        if (opts.pokemon) {
           console.log(title + ' versions you haven\'t installed yet:');
           versions = _.difference(versions, installed);
         }
-        printVersions(versions, function(err){
+        printVersions(versions, function(err) {
           abortIfError(err);
         });
       });
     });
   },
-  path: function(){
-    mvm.path(function(err, p){
+  path: function() {
+    mvm.path(function(err, p) {
       console.log(p);
     });
   },
-  kill: function(){
-    mvm.kill(function(){});
+  kill: function() {
+    mvm.kill(function() {});
   },
-  shell: function(){
-    mvm.path(function(){
-      spawn(which.sync('mongo'), [], {stdio: 'inherit'});
+  shell: function() {
+    mvm.path(function() {
+      spawn(which.sync('mongo'), [], {
+        stdio: 'inherit'
+      });
     });
   },
-  d: function(){
-    spawn(which.sync('mongod'), [], {stdio: 'inherit'});
+  d: function() {
+    spawn(which.sync('mongod'), [], {
+      stdio: 'inherit'
+    });
   },
-  use: function(opts){
-    mvm.use(opts, function(err){
+  use: function(opts) {
+    mvm.use(opts, function(err) {
       abortIfError(err);
-      mvm.current(function(err, v){
+      mvm.current(function(err, v) {
         abortIfError(err);
         console.log('switched to ' + v);
       });
     });
   },
-  list: function(){
-    mvm.installed(function(err, versions){
+  list: function() {
+    mvm.installed(function(err, versions) {
       abortIfError(err);
 
-      if(versions.length > 0){
-        return printVersions(versions, function(err){
-          if(err) return console.log(err) && process.exit(1);
+      if (versions.length > 0) {
+        return printVersions(versions, function(err) {
+          if (err) return console.log(err) && process.exit(1);
         });
       }
 
       console.log('0 versions installed.  Run one of:');
 
-      mvm.resolve([{version: 'unstable'}, {version: 'stable'}], function(err, data){
-        abortIfError(err);
+      mvm.resolve([{
+        version: 'unstable'
+        }, {
+        version: 'stable'
+      }], function(err, data) {
+          abortIfError(err);
 
-        console.log('    m use stable; # installs MongoDB v' + data.stable.version);
-        console.log('    m use unstable; # installs MongoDB v' + data.unstable.version);
-      });
+          console.log('    m use stable; # installs MongoDB v' + data.stable.version);
+          console.log('    m use unstable; # installs MongoDB v' + data.unstable.version);
+        });
     });
   }
 };
@@ -126,7 +154,7 @@ var opts = {
   branch: argv['--branch']
 };
 
-var cmd = Object.keys(commands).filter(function(name){
+cmd = Object.keys(commands).filter(function(name) {
   return argv[name] === true;
 })[0] || (argv['<version>'] ? 'use' : 'list');
 debug('cmd is `%s` with opts `%j`', cmd, opts);
